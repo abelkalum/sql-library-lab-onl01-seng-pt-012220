24897452e76ec0bf6060f3f1a0a652d6ff807d9a
Abel Kalum <chumbabel@gmail.com>
2020-03-30 18:53:16 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -17,6 +17,7 @@ end
 
 def select_series_title_with_most_human_characters
    "SELECT series.title, character.species FROM series JOIN characters ON series.author_id = characters.author_id GROUP BY series.title ORDER BY COUNT(species) WHERE species = 'human' DESC LIMIT 1;"
+   
 end
 
 def select_character_names_and_number_of_books_they_are_in
