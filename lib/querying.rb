717e5131e91bab7afb219bac17a5399b5f3cf17d
Abel Kalum <chumbabel@gmail.com>
2020-03-30 19:22:19 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -21,4 +21,5 @@ end
 
 def select_character_names_and_number_of_books_they_are_in
   "SELECT character_id, book_id AS character.name, book.title FROM character_books JOIN characters ON character.id = character_books.character_id JOIN books ON books.id = character_books.book_id GROUP BY character.name;"
+  
 end
